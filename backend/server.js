59986090ffa47f6988d232e91cf9c0cc2cfccc21
S-kaif123 <kaifbhombal.kb@gmail.com>
2025-11-17 11:24:06 +0530

    Started designing using shadcn

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,28 @@
+import dotenv from "dotenv";
 import express from "express";
-const app= express();
+import { ConnectToDB } from "./database/db.js";
+import { log } from "console";
+import cors from "cors";
+dotenv.config();
+const app = express();
 app.use(express.json());
+ConnectToDB();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use((err, req, res, next) => {
+  log(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong in server section",
+  });
+});
+const PORT = process.env.PORT;
+app.listen((req, res) => {
+  console.log(`server running at PORT ${PORT}`);
+});
