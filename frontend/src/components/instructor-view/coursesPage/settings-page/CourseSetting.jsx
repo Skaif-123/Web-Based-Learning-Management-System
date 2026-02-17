e8ed73ad9f9a058a8a77b901265afb8bbb5f7199
S-kaif123 <kaifbhombal.kb@gmail.com>
2026-02-17 15:19:38 +0530

    Completed Course Settings Tab

--- frontend/src/components/instructor-view/coursesPage/settings-page/CourseSetting.jsx
+++ frontend/src/components/instructor-view/coursesPage/settings-page/CourseSetting.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-dropdown-menu";
 
 const CourseSetting = () => {
   return (
-    <div>CourseSetting</div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Course Settings</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-3">
+          <Label>Upload Course Image</Label>
+          <Input type="file" accept="image/*" />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default CourseSetting
+export default CourseSetting;
