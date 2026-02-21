38e0f32eed11b06b1a7131c201234e32dee2249d
S-kaif123 <kaifbhombal.kb@gmail.com>
2026-02-21 15:38:00 +0530

    Completed course-controllers

--- backend/controllers/auth-controller/instructor-controller/course-controller.js
+++ backend/controllers/auth-controller/instructor-controller/course-controller.js
@@ -2,6 +2,16 @@ import Course from "../../models/Course.js";
 
 const addNewCourse = async (req, res) => {
   try {
+    const courseData = req.body;
+    const newlyCreatedCourse = new Course(courseData);
+    const saveCourse = await newlyCreatedCourse.save();
+    if (saveCourse) {
+      res.status(201).json({
+        success: true,
+        message: "Course Saved Successfully",
+        data: saveCourse,
+      });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -10,8 +20,15 @@ const addNewCourse = async (req, res) => {
     });
   }
 };
+
 const getAllCourses = async (req, res) => {
   try {
+    const coursesList = await Course.find({});
+
+    res.status(200).json({
+      success: true,
+      data: coursesList,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -20,8 +37,22 @@ const getAllCourses = async (req, res) => {
     });
   }
 };
-const getCourseDetails = async (req, res) => {
+
+const getCourseDetailsById = async (req, res) => {
   try {
+    const { id } = req.params;
+    const courseDetails = await Course.findById(id);
+    if (!courseDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: courseDetails,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -30,8 +61,30 @@ const getCourseDetails = async (req, res) => {
     });
   }
 };
+
 const updateCourseById = async (req, res) => {
   try {
+    const { id } = req.params;
+    const updatedCourseData = req.body;
+    const updatedCourse = await Course.findByIdAndUpdate(
+      id,
+      updatedCourseData,
+      {
+        new: true,
+      },
+    );
+
+    if (!updatedCourse) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Course Updated  Successfully",
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
